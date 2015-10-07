2305469a8a4fa5ad7ddad3e12bfca17c5b0ec23d
PerlaGCastillo <contactoperlagodinez@gmail.com>
2015-10-07 10:29:56 -0500

    agregando base.html, documentación.xls+numbers, index.html, index.php=>> creamos el código para hacer funcionar el login de forma básica

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php /*esto inicia un bloque de código en php*/
+
+echo "funciona php"; 
+    /*echo nos permite imprimir resultados 
+    "como en el document.write"*/
+
+
+/*esto inicia un bloque de código en php*/
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+     <link rel="stylesheet" href="css/main.css"> 
+   <title>index</title>
+</head>
+
+<body>
+    <header> <!-- encabezado -->
+                <aside> <span class="naranja">Mi primer login</span> </aside>
+                <nav>
+                    <a class="activo" href="index.html">Inicio</a>
+                    |   
+                    <a href=" ">Ingresar</a>
+                    |
+                    <a href=" ">Nuevo usuario</a>
+                </nav>
+    </header>
+
+        <div id="contenido">
+            <p > felicidades, bienvenidoas a mi mundo </p><!--termina contenido-->
+        <footer>
+             <p>Copyleft: TDW2.0</p>
+        </footer>
+</body>
+</html>
